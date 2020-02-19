ea74d08634dbfff0542c4e8b4bfca6859079fef8
IvanPolienko <piar1986@yandex.ru>
2020-02-20 00:56:28 +0700

    Update parse_tululu_category.py

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -7,78 +7,77 @@ from pathlib import Path
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from urllib.parse import urljoin
+from pprint import pprint
+
+Path('books').mkdir(parents=True, exist_ok=True)
+Path('images').mkdir(parents=True, exist_ok=True)
 
 url_page_template = 'http://tululu.org/l55/{}'
-url_book_template = 'http://tululu.org/b{}/'
+#url_book_template = 'http://tululu.org/b{}/'
 url_download_txt_template = 'http://tululu.org/txt.php?id={}'
 
-def download_txt(url, filename, folder='books/'):
+def download_txt(url, filename, folder='books'):
 	response = requests.get(url, allow_redirects = False)
 	response.raise_for_status()
 	if response.status_code==200:
 		filepath = os.path.join(folder, sanitize_filename(filename + '.txt'))
-		#with open(filepath, 'w') as file:
-		#	file.write(response.text)
-		#return filepath
+		with open(filepath, 'w') as file:
+			file.write(response.text)
+		return filepath
 	
 
-def download_image(url, filename, folder='images/'):
+def download_image(url, filename, folder='images'):
 	response = requests.get(url, allow_redirects = False)
 	response.raise_for_status()
 	if response.status_code==200:
-		#soup = BeautifulSoup(response.text, 'lxml')
-		#image = soup.find('div', class_="bookimage").find('img')['src']
-		#image_url = urljoin('http://tululu.org/more', image)
-		#print(image.split('/'))
 		filepath = os.path.join(folder, filename)
-		#response = requests.get(image_url)
-		#response.raise_for_status()
 		with open(filepath, 'wb') as file:
 			file.write(response.content)
+		return filepath	
 
-for n in range(1):
-	url = url_page_template.format(n + 1)
+books_data = []
+
+for n in range(1,2):
+	url = url_page_template.format(n)
 	response = requests.get(url)
 	response.raise_for_status()
 	soup = BeautifulSoup(response.text, 'lxml')
-	book_href = soup.find_all('div', class_="bookimage")
+	books_from_page = soup.select(".bookimage")
+	
+	for book in books_from_page:
+		book_page_href = book.find('a')['href']
+		book_page_url = urljoin('http://tululu.org/more', book_page_href)
 
-	for book in book_href:
-		href = book.find('a')['href']
-		book_id = href.strip('/b')
-		href = book.find('img')['src']
-
-		response = requests.get(url_book_template.format(book_id), allow_redirects = False)
+		response = requests.get(book_page_url, allow_redirects = False)
 		response.raise_for_status()
 		soup = BeautifulSoup(response.text, 'lxml')
-		title_tag = soup.find('h1')
-		print(title_tag)
-		title_text = title_tag.text
-		text = title_text.split('::')
-		book_title = text[0].strip()
-		book_author = text[1].strip()
-		'''
-		comments = soup.find_all('div', class_="texts")
-		for comment in comments:
-			print(comment.find('span').text)
 
-		genres = soup.find('span', class_="d_book").find_all('a')
-		for genre in genres:
-			print(genre.text)
-		'''
-		book_url = urljoin('http://tululu.org/more', href)
-		book_url_txt = url_download_txt_template.format(book_id)
-		#download_txt(book_url_txt, book_title)
-		href = book.find('img')['src'].strip('/shots')
-		download_image(book_url, href)
+		book_title_text = soup.select_one("h1").text
+		book_information = book_title_text.split('::')
+		book_title = book_information[0].strip()
+		book_author = book_information[1].strip()
 		
-'''
-{
-	"title":
-	"author":
-	"img_src":
-	"book_path":
-	"comments":
-	"genres":
-}
-'''
+		comments = [comment.text for comment in soup.select(".texts span")]
+		genres = [genre.text for genre in soup.select("span.d_book a")]
+		
+		book_id = book_page_href.strip('/b')
+		book_txt_url = url_download_txt_template.format(book_id)
+		book_path = download_txt(book_txt_url, book_title)
+
+		book_image_src = book.find('img')['src']
+		image_title = book_image_src.split('/')[2]
+		book_image_url = urljoin('http://tululu.org/more', book_image_src)
+		img_src = download_image(book_image_url, image_title)
+
+		book_data = {
+				"title": book_title,
+				"author": book_author,
+				"img_src": img_src,
+				"book_path": book_path,
+				"comments": comments,
+				"genres": genres
+				}
+		books_data.append(book_data)
+
+with open("books_data.json", "w") as my_file:
+  json.dump(books_data, my_file, ensure_ascii = False, indent=2)
